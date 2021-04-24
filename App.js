0c1e81ff2a6c5ebf17d8a501482dc87ca95495df
Fah Jin <yongfahjin@hotmail.com>
2021-04-24 14:00:37 +0800

    before sql

--- App.js
+++ App.js
@@ -1,72 +1,26 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { createStackNavigator, HeaderTitle } from "@react-navigation/stack";
 import * as SQLite from "expo-sqlite";
-import { useEffect } from "react";
-import Entypo from "react-native-vector-icons/Entypo";
+import NotesStack from "./screens/NotesStack";
+import AddScreen from "./screens/AddScreen";
 
 const db = SQLite.openDatabase("notes.db")
-
-function NotesScreen({ navigation }) {
- useEffect(() => {
-   navigation.setOptions({
-    headerRight: () => (
-      <TouchableOpacity onPress={addNote}>
-        <Entypo 
-        name="new-message"
-        size={24}
-        color="black"
-        style={{marginRight: 20}}
-        />
-        </TouchableOpacity>
-   ),
- });
-});
-
-function addNote() {
-  console.log("Add Note");
-}
-return <View style={styles.container}></View>;
-}
-
 const Stack = createStackNavigator();
 
 export default function App() {
  return (
    <NavigationContainer>
-     <Stack.Navigator>
+     <Stack.Navigator mode="modal" headerMode="none">
        <Stack.Screen
-         name="Notes"
-         component={NotesScreen}
-         options={{
-           headerTitle: "Notes App",
-           headerTitleStyle: {
-             fontWeight: "bold",
-             fontSize: 30,
-           },
-           headerStyle: {
-             height: 120,
-             backgroundColor: "yellow",
-             borderBottomColor: "#ccc",
-             borderBottomWidth: 1,
-           },
-         }}
-       />
+         name="Notes Stack"
+         component={NotesStack}
+         options={{ headerShown:false}}
+         />
+         <Stack.Screen name ="Add Note" component={AddScreen} />
      </Stack.Navigator>
    </NavigationContainer>
  );
 }
-
-
-
-const styles = StyleSheet.create({
- container: {
-   flex: 1,
-   backgroundColor: "#ffc",
-   alignItems: "center",
-   justifyContent: "center",
- },
-});
 
 
---
+++ screens/NotesScreen.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from "react";
+import {
+ StyleSheet,
+ Text,
+ View,
+ FlatList,
+ TouchableOpacity,
+ 
+} from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+export default function NotesScreen({ route, navigation }) {
+    const [notes, setNotes] = useState([
+      {title: "hello world", done: false, id: "0"},
+      {title: "buy breakfast", done: false, id: "1"}
+    ]);
+  
+   useEffect(() => {
+     navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={addNote}>
+          <Entypo 
+          name="add-to-list"
+          size={24}
+          color="black"
+          style={{marginRight: 20}}
+          />
+          </TouchableOpacity>
+     ),
+   });
+  });
+  
+  useEffect(() => {
+    if (route.params?.text) {
+      
+ 
+      const newNote = {
+        title: route.params.text,
+        done: false,
+        id: notes.length.toString(),
+      };
+      setNotes([...notes, newNote]);
+    }
+  }, [route.params?.text]);
+ 
+ 
+  function addNote() {
+    navigation.navigate("Add Note");
+ }
+  
+  function renderItem({ item }) {
+    return (
+      <View
+        style={{
+          padding: 10,
+          paddingTop: 20,
+          paddingBottom: 20,
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        }}
+      >
+        <Text style={{ textAlign: "left", fontSize: 16 }}>{item.title}</Text>
+      </View>
+    );
+  }
+  
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={{ width: "100%" }}
+        data={notes}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+  }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#ccccff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+   });
+   
+   
+   
+
+
+
